df3c018342f97e04b7541e79736ac2dfde16083b
himayajawad <himaya19jawad@gmail.com>
2026-07-01 18:24:05 -0600

    creating flexible components and setting dynamic component types

--- src/components/TabButtons.jsx
+++ src/components/TabButtons.jsx
@@ -2,6 +2,7 @@ import TabButton from "./TabButton";
 import { useState } from "react";
 import { EXAMPLES } from "../data.js";
 import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 export default function TabButtons(props) {
 
     const tabButtonTitle = ["Components", "JSX", "Props", "State"];
@@ -13,30 +14,33 @@ export default function TabButtons(props) {
     return (
         <Section id="examples" title="Examples">
 
-            <menu>
-                {tabButtonTitle.map((title) => {
+             <Tabs
+             buttonsContainer={Section}
+            buttons={<>
+               {tabButtonTitle.map((title) => {
 
                     return (
                         <TabButton
                             key={title}
                             isSelected={stateOfButton === title.toLowerCase()}
-                            onClick={() => buttonClickHandler(title.toLowerCase())}
+                           onClick={() => buttonClickHandler(title.toLowerCase())}
                         >
                             {title}
-                        </TabButton>
+                    </TabButton>
                     );
-                })}
-
-            </menu>
-            {
-                <div id="tab-content">
-                    <h3>{EXAMPLES[stateOfButton.toLowerCase()].title}</h3>
-                    <p>{EXAMPLES[stateOfButton.toLowerCase()].description}</p>
-                    <pre>
-                        <code>{EXAMPLES[stateOfButton.toLowerCase()].code}</code>
-                    </pre>
-                </div>
-            }
+               })}
+         </>}
+            >
+                {
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[stateOfButton.toLowerCase()].title}</h3>
+                        <p>{EXAMPLES[stateOfButton.toLowerCase()].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[stateOfButton.toLowerCase()].code}</code>
+                        </pre>
+                    </div>
+                } 
+        </Tabs>
 
         </Section>
     );
